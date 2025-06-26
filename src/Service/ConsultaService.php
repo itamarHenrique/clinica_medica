a7c897d9baf907edfe5aede2f7aee13326a90db9
itamarHenrique <itamarhjp1@gmail.com>
2025-06-26 10:52:06 -0300

    feat: Adição do metodo de criar consulta

--- src/Service/ConsultaService.php
+++ src/Service/ConsultaService.php
@@ -4,6 +4,8 @@ namespace App\Service;
 
 use App\Dto\ConsultaResource;
 use App\Entity\Consulta;
+use App\Entity\Medico;
+use App\Entity\Paciente;
 use App\Repository\ConsultaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
@@ -25,5 +27,41 @@ class ConsultaService{
         return array_map(function($consulta) {
             return ConsultaResource::fromEntity($consulta);
         }, $consultas);
+    }
+
+    public function criarConsulta(int $pacienteId, int $medicoId, \DateTimeInterface $data, \DateTimeInterface $horario): Consulta
+    {
+        $paciente = $this->entityManagerInterface->getRepository(Paciente::class)->find($pacienteId);
+        $medico = $this->entityManagerInterface->getRepository(Medico::class)->find($medicoId);
+
+        if(!$paciente){
+            throw new \InvalidArgumentException("Paciente não encontrado!");
+        } elseif(!$medico){
+            throw new \InvalidArgumentException("Medico não encontrado!");
+        }
+
+        $conflito = $this->entityManagerInterface->getRepository(Consulta::class)->findOneBy([
+            'data' => $data,
+            'horario' => $horario,
+            'medico' => $medico
+        ]);
+
+        if($conflito){
+            throw new \Exception('Já existe uma consulta para este medico nesse dia e horario.');
+        }
+
+        $consulta = new Consulta();
+        $consulta->setPaciente($paciente);
+        $consulta->setHorario($horario);
+        $consulta->setData($data);
+        $consulta->setMedico($medico);
+        $consulta->setStatus('agendada');
+        $consulta->setCriadoEm(new \DateTime());
+        $consulta ->setAtualizadoEm(new \DateTime());
+
+        $this->entityManagerInterface->persist($consulta);
+        $this->entityManagerInterface->flush();
+
+        return $consulta;
     }
 }
